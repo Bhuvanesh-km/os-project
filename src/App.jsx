c291384f7c70a30f139c84877594f328f2743b46
Bhuvanesh-km <bhuvaneshkm441@gmail.com>
2024-05-03 10:26:43 +0530

    msal-auth

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsal,
+  MsalProvider,
+} from "@azure/msal-react";
+import { loginRequest } from "./auth-config";
+
 const Brita = lazy(() => import("./pages/Brita.jsx"));
 const Burtsbees = lazy(() => import("./pages/Burtsbees.jsx"));
 const CatLitter = lazy(() => import("./pages/CatLitter.jsx"));
@@ -14,26 +22,58 @@ const Glad = lazy(() => import("./pages/Glad.jsx"));
 const HomeCare = lazy(() => import("./pages/HomeCare.jsx"));
 const Laundry = lazy(() => import("./pages/Laundry.jsx"));
 
-function App() {
+const WrappedView = () => {
+  const { instance } = useMsal();
+  const activeAccount = instance.getActiveAccount();
+
+  const handleRedirect = () => {
+    instance
+      .loginRedirect({
+        ...loginRequest,
+        prompt: "create",
+      })
+      .catch((e) => console.error(e));
+  };
+  return (
+    <div className="App">
+      <AuthenticatedTemplate>
+        {activeAccount && (
+          <>
+            <h1>Welcome {activeAccount.name}</h1>
+            <Router>
+              <Suspense fallback={<h1>Loading...</h1>}>
+                <NavBar />
+                <Routes>
+                  <Route path="/" element={<Brands />} />
+                  <Route path="/brita" element={<Brita />} />
+                  <Route path="/burtsbees" element={<Burtsbees />} />
+                  <Route path="/cat-litter" element={<CatLitter />} />
+                  <Route path="/clorox" element={<Clorox />} />
+                  <Route path="/glad" element={<Glad />} />
+                  <Route path="/homecare" element={<HomeCare />} />
+                  <Route path="/laundry" element={<Laundry />} />
+                  <Route path="*" element={<h1>Not Found</h1>} />
+                </Routes>
+                <Footer />
+              </Suspense>
+            </Router>
+          </>
+        )}
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <h1>Not signed in</h1>
+        <button onClick={handleRedirect}>Sign In</button>
+      </UnauthenticatedTemplate>
+    </div>
+  );
+};
+
+function App({ instance }) {
   return (
     <>
-      <Router>
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Brands />} />
-            <Route path="/brita" element={<Brita />} />
-            <Route path="/burtsbees" element={<Burtsbees />} />
-            <Route path="/cat-litter" element={<CatLitter />} />
-            <Route path="/clorox" element={<Clorox />} />
-            <Route path="/glad" element={<Glad />} />
-            <Route path="/homecare" element={<HomeCare />} />
-            <Route path="/laundry" element={<Laundry />} />
-            <Route path="*" element={<h1>Not Found</h1>} />
-          </Routes>
-          <Footer />
-        </Suspense>
-      </Router>
+      <MsalProvider instance={instance}>
+        <WrappedView />
+      </MsalProvider>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,27 @@ import App from "./App";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+import { msalConfig } from "./auth-config";
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+if (
+  !msalInstance.getActiveAccount() &&
+  msalInstance.getAllAccounts().length > 0
+) {
+  msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+}
+
+msalInstance.addEventCallback((event) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+    const account = event.payload.account;
+    msalInstance.setActiveAccount(account);
+  }
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <App instance={msalInstance} />
   </React.StrictMode>
 );
